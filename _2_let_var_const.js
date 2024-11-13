cde7975d747d7834bad5e26a266523245fac5513
Shashi Singh <shashisingh92292@gmail.com>
2024-11-13 21:33:49 +0530

    2nd commit

---
+++ _2_let_var_const.js
@@ -0,0 +1,3 @@
+let a = 10; // this will be locally declared 
+var b = 20; // this will be globally declared 
+const c = 30; // this is just const cannot be changed
---
+++ _3_PracticeSet_1.js
@@ -0,0 +1,13 @@
+let a = String("Shashi")
+let b = 10
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(a+b)
+
+const dictionary = {
+    "lol":10
+}
+
+dictionary["asd"] = "20"
+console.log(dictionary["asd"])
