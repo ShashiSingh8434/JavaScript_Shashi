4d5c0bd2543a8a0e1ccd18156c8b6ff6894b9a53
Shashi Singh <shashisingh92292@gmail.com>
2024-11-14 00:11:57 +0530

    3rd commit

---
+++ _6_Functions.js
@@ -0,0 +1,12 @@
+function lolsum(a,b){
+    return a+b;
+}
+const lols = (a,b)=>{
+    return a+b;
+}
+const greet = ()=>{
+    console.log("Hello man what's up!!!");
+}
+console.log(lolsum(1,2))
+console.log(lols(1,2))
+greet()
---
+++ _7_PracticeSet_3.js
@@ -0,0 +1,9 @@
+const obj = {
+    "one": 10,
+    "two": 20,
+    "three": 30
+}
+
+for(let a in obj){
+    console.log(a+" value is: "+obj[a])
+}
